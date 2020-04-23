113cab0b14d5674425247bc57c3c494007f65c6d
erniberni <1084946+erniberni@users.noreply.github.com>
2020-04-23 20:21:33 +0200

    Update install_script.sh

--- install_script.sh
+++ install_script.sh
@@ -14,6 +14,8 @@ wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | sudo apt-key add -
 echo "deb http://deb.anydesk.com/ all main" | sudo tee /etc/apt/sources.list.d/anydesk-stable.list
 sudo apt update
 sudo apt -y install anydesk
+# Autostart für Aktualisierungsverwaltung ausschalten
+sudo rm /etc/xdg/autostart/update-notifier.desktop
 # autostart 
 echo "[Desktop Entry]"|sudo tee /etc/xdg/autostart/jitsiStart.desktop 
 echo "Type=Application"|sudo tee -a /etc/xdg/autostart/jitsiStart.desktop 
